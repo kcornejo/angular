8ddf2e7bd01feb0515a3b6ef82ab31389905e20d
kcornejo <kcornejo@velfasa.com>
2018-10-24 17:33:09 -0600

    Avances

---
+++ ts-hello/main.ts
@@ -0,0 +1,15 @@
+function log(message){
+    console.log(message);
+}
+var message = 'Hello World';
+
+log(message);
+
+
+function doSomething(){
+    for (let i = 0; i < 5; i++){
+        console.log(i);
+    }
+    console.log(`Finaly ` + i);
+}
+doSomething();
